d657783b4878286729101812c018461208e06858
swardeo <swardeo@pm.me>
2021-02-05 16:52:08 +0000

    Removed unused dependencies

--- bloom-core/src/main/java/provider/MapperProvider.java
+++ bloom-core/src/main/java/provider/MapperProvider.java
@@ -1,11 +1,10 @@
 package provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class MapperProvider {
 
     public static ObjectMapper provideMapper() {
-        return new ObjectMapper().registerModule(new JavaTimeModule());
+        return new ObjectMapper();
     }
 }
